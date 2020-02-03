d5418e31f4a4cd1309ed63713e57fff57224358a
Patrick Du <patrickduu@gmail.com>
2020-02-02 21:19:08 -0500

    subclass Developer

--- oop.py
+++ oop.py
@@ -32,17 +32,39 @@ class Employee:
             return False
         return True
 
-class Developer(Employee): # Class inherits from Employee class
+class Developer(Employee): # Subclass that inherits from Employee
     raise_amt = 1.10
     
     def __init__(self, first, last, pay, prog_lang):
-        super().__init__(first, last, pay) # Super handles init method for first, last, and pay by utilizing the init from Employee
+        super().__init__(first, last, pay) # Super handles init method by utilizing the Employee init
         self.prog_lang = prog_lang
 
-#print(help(Developer)) # Help functions shows method resolution order (places Python searches for attributes and methods)
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+        
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+    
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullName())
 
+
+# Instance of Employee, Developer, Manager
 emp_1 = Employee('Corey', 'Schafer', 50000)
 dev_1 = Developer('Test', 'User', 60000, 'Python')
+dev_2 = Developer('Test2', 'User2', 60000, 'Python')
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
 
 
 # Testing Employee Class Method
@@ -55,8 +77,31 @@ import datetime
 my_date = datetime.date(2020, 2, 2)
 print(Employee.is_workday(my_date))
 
+
 # Testing Developer Class 
 print(dev_1.pay)
 dev_1.apply_raise()
 print(dev_1.pay)
+
+
+# Help functions shows method resolution order (places Python searches for attributes and methods)
+#print(help(Developer)) 
+
+
+# Testing Manager Class
+print(mgr_1.email)
+mgr_1.add_emp(dev_2)
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emps()
+
+
+# isInstance() tells us whether an object is an instance of a class
+print(isinstance(mgr_1, Manager))
+print(isinstance(mgr_1, Employee))
+print(isinstance(mgr_1, Developer))
+
+# issubclass() tells us whether a class is a subclass of another
+print(issubclass(Developer, Employee))
+print(issubclass(Manager, Employee))
+print(issubclass(Manager, Developer))
 
